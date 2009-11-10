ffb4241e970db76899a20abe13b4cec2ff97fd03
Mohammed Taher <mohammed.taher@eloqua.com>
2009-11-10 16:56:53 -0500

    major upload refacator and added functionality

--- frameworks/foundation/views/upload.js
+++ frameworks/foundation/views/upload.js
@@ -1,42 +1,30 @@
-// ==========================================================================
-// Project:   Scui.Upload
-// Copyright: ©2009 My Company, Inc.
-// ==========================================================================
-/*globals SCUI */
+// ========================================================================
+// SCUI.Upload
+// ========================================================================
 
 /** @class
 
-  View that allows the user to upload a file to a specific service
-  
-  TODO: [MT] currently this doesn't do any error checking. The service has
-  to be updated first
+  A simple view that allows the user to upload a file to a specific service.
   
   @extends SC.View
   @author Mohammed Taher
 */
+
 SCUI.Upload = SC.View.extend(
 /** @scope Scui.Upload.prototype */ {
   
   /*
     URI of Restful service
   */
-  formAction: null,
+  uploadTarget: null,
   
-  /*
-    Private variable that's updated based on the values of uploadStartMessage and
-    uploadCompleteMessage.
+  /**
+    Status of the current upload. Can be one of 3 values,
+      1. 'READY'
+      2. 'IN-PROGRESS'
+      3. 'DONE'
   */
-  _uploadStatus: '',
-  
-  /*
-    String to be displayed when the the upload starts
-  */
-  uploadStartMessage: 'Uploading...',
-  
-  /*
-    String to be displayed when the upload is done
-  */
-  uploadCompleteMessage: 'Upload Complete',
+  status: '',
   
   /*
     Specify these to allow for any extra action to be taken once the upload
@@ -45,102 +33,75 @@ SCUI.Upload = SC.View.extend(
   uploadSuccessfullTarget: null,
   uploadSuccessfullAction: null,
   
-  displayProperties: 'action _uploadStatus'.w(),
+  /**
+    The input's preceding label
+  */
+  label: 'File: ',
+  
+  displayProperties: 'uploadTarget'.w(),
 
   render: function(context, firstTime) {
-    
     var frameId = this.get('layerId') + 'Frame';
-    var formAction = this.get('formAction');
-    var uploadStatus = this.get('_uploadStatus');
+    var uploadTarget = this.get('uploadTarget');
+    var label = this.get('label');
     
     if (firstTime) {
-      
-      context.push('<form method="post" enctype="multipart/form-data" target="' + frameId + '" action="' + formAction + '">');
-      context.push('File: <input type="file" name="Filedata" />');
+      context.push('<form method="post" enctype="multipart/form-data" target="' + frameId + '" action="' + uploadTarget + '">');
+      context.push(label + '<input type="file" name="Filedata" />');
       context.push('</form>');
-      
       context.push('<iframe frameBorder="0" src="#" id="' + frameId + '" name="' + frameId + '" style="width:0; height:0;"></iframe>');
       
-      context.push('<br>');
-      context.push('<br>');
-      
-      context.push('<span class="upload-status">' + uploadStatus + '</span>');
-      
     } else {
-      this.$('.upload-status').text(uploadStatus);
+      this.$('form').get(0).action = uploadTarget;
+      
     }
-    
     sc_super();
-  },
-  
-  createChildViews: function() {
-    var childViews = [], view;
-    
-    view = this.createChildView(
-      SC.ButtonView.design({
-        theme: 'capsule',
-        layout: { left: 20, width: 80, top: 30, height: 24 },
-        title: 'Upload',
-        target: this,
-        action: function() {
-          this.get('parentView').$('form').get(0).submit();
-        }
-      }),
-      { rootElementPath: [0] }
-    );
-    childViews.push(view);
-    
-    this.set('childViews', childViews);
   },
   
   didCreateLayer: function() {
     sc_super();
-    
     var frame = this.$('iframe');
-    SC.Event.add(frame, 'load', this, this.iframeDidLoad);
-    
-    var form = this.$('form');
-    SC.Event.add(form, 'submit', this, this.formDidSubmit);
+    SC.Event.add(frame, 'load', this, this._uploadDone);
 
-    // in IE the frame load event is fired when the frame first loads and 
-    // when the iframe is redirected to the servcie, while in firefox, it's only
-    // fired when the iframe is redirected. This ensures that the load is not
-    // fired the first time around in IE.
-    if (SC.browser.msie) {
-      this._ignoreFrameDidLoadCallback = YES;
-    } else {
-      this._ignoreFrameDidLoadCallback = NO;
-    }
   },
   
   willDestroyLayer: function() {
     var frame = this.$('iframe');
-    SC.Event.remove(frame, 'load', this, this.iframeDidLoad);
-    
-    var form = this.$('form');
-    SC.Event.remove(form, 'submit', this, this.formDidSubmit);
-    
+    SC.Event.remove(frame, 'load', this, this._uploadDone);
     sc_super();
   },
   
-  formDidSubmit: function() {
-    var uploadStartMessage = this.get('uploadStartMessage');
-    this.set('_uploadStatus', uploadStartMessage);
+  /**
+    Starts the file upload (by submitting the form) and alters the status from READY to IN-PROGRESS.
+  */
+  startUpload: function() {
+    this.$('form').get(0).submit();
+    this.set('status', 'IN-PROGRESS');
   },
   
-  iframeDidLoad: function() {
-    if (!this._ignoreFrameDidLoadCallback) {
-      var uploadCompleteMessage = this.get('uploadCompleteMessage');
+  /**
+    Clears the file upload by regenerating the HTML. This is guaranateed
+    to work across all browsers. Also resets the status to READY.
+  */
+  clearFileUpload: function() {
+    if (this.$('form').length > 0) {
+      this.$('form').get(0).innerHTML = this.$('form').get(0).innerHTML;
+      this.set('status', 'READY');
+    }
+  },
+  
+  /**
+    This function is called when the upload is done and the iframe loads. It'll
+    execute the uploadSuccessfullAction/uploadSuccessfullTarget function and change
+    the status from IN-PROGRESS to DONE.
+  */
+  _uploadDone: function() {
+    this.set('status', 'DONE');
 
-      this.set('_uploadStatus', uploadCompleteMessage);
-
-      var action = this.get('uploadSuccessfullAction');
-      var target = this.get('uploadSuccessfullTarget');
-      if (action && target) {
-        this.getPath('pane.rootResponder').sendAction(action, target, this, this.get('pane'));
-      }      
-    } else {
-      this._ignoreFrameDidLoadCallback = NO;
+    var action = this.get('uploadSuccessfullAction');
+    var target = this.get('uploadSuccessfullTarget');
+    if (action && target) {
+      this.getPath('pane.rootResponder').sendAction(action, target, this, this.get('pane'));
     }
   } 
 

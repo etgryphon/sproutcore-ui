3ff82689a0350cb1cde87ccb871e0d82f808f9da
Jonathan Lewis <jslewis1@gmail.com>
2009-11-24 11:22:58 -0500

    Replacing previous dashboard files with new ones.  Added widget view delegate support.

--- frameworks/dashboard/mixins/dashboard_delegate.js
+++ frameworks/dashboard/mixins/dashboard_delegate.js
@@ -5,223 +5,36 @@
 sc_require('core');
 
 /** @mixin
-  
-  TODO: [EG] Add Documentation
-  
-  NOTE: This mixin must be used in conjunction with the SC.CollectionRowDelegate, SCUI.DynamicListItem mixin on the 
-        list item views and SCUI.DynamicCollection on the view.
 
+  Mixin for delegates of SCUI.DashboardView so that it can request unique
+  widget views on a per widget basis.
+
+  @author Jonathan Lewis
   @author Evin Grano
 */
 
-SCUI.DashboardDelegate = SC.merge( SC.CollectionViewDelegate, SCUI.CollectionViewDynamicDelegate, {
-  
-  dashboardWidgetExampleViewFor: function(view, content, contentIndex) {
-    return null ;
-  },
-  
-  /******************************************************************************************************************
-    Collection View Dynamic Delegate Code...
-  *******************************************************************************************************************/
-  
+SCUI.DashboardDelegate = {
+
+  // PUBLIC PROPERTIES
+
+  isDashboardDelegate: YES,
+
+  // PUBLIC METHODS
+
   /**
-    just to make the names appear better
+    Called by the SCUI.DashboardView when it needs a widget view class
+    for a particular widget item.  Returning null causes SCUI.DashboardView
+    to use the default SCUI.WidgetMissingView for that widget.
+    
+    dashboardView: the calling dashboard view.
+    content: the widget that needs a widget view.
+    contentIndex: the index of 'content' in the widget array.
+
+    Return null or a view class.
   */
-  collectionViewContentExampleViewFor: function(view, content, contentIndex) {
-    if (content) return this.dashboardWidgetExampleViewFor(view, content, contentIndex) ;
+  dashboardWidgetViewFor: function(dashboardView, content, contentIndex) {
+    console.log('%@.dashboardWidgetViewFor(content: %@, index: %@)'.fmt(this, content, contentIndex));
     return null;
-  },
-  
-  /******************************************************************************************************************
-    Collection View Delegate Code...
-  *******************************************************************************************************************/
-  
-  /**
-    Called by the collection view whenever the deleteSelection() method is
-    called.  You can implement this method to get fine-grained control over
-    which items can be deleted.  To prevent deletion, return null.
-    
-    This method is only called if canDeleteContent is YES on the collection
-    view.
-    
-    @param {SC.CollectionView} view the collection view
-    @param {SC.IndexSet} indexes proposed index set of items to delete.
-    @returns {SC.IndexSet} index set allowed to delete or null.
-  */
-  collectionViewShouldDeleteIndexes: function(view, indexes) { 
-    //console.log('DashboardView: collectionViewShouldDeleteIndexes');
-    return indexes; 
-  },
-  
-  // ..........................................................
-  // DRAG SOURCE SUPPORT
-  //
-  
-  /**
-    Called by the collection view just before it starts a drag to give you
-    an opportunity to decide if the drag should be allowed. 
-    
-    You can use this method to implement fine-grained control over when a 
-    drag will be allowed and when it will not be allowed.  For example, you
-    may enable content reordering but then implement this method to prevent
-    reordering of certain items in the view.
-    
-    The default implementation always returns YES.
-    
-    @param view {SC.CollectionView} the collection view
-    @returns {Boolean} YES to alow, NO to prevent it
-  */
-  collectionViewShouldBeginDrag: function(view) {
-    //console.log('SCUI.DashboardDelegate: collectionViewShouldBeginDrag');
-    var sel = view.get('selection');
-    var isLocked = NO;
-    if (sel){
-      // find only the indexes that are in both dragContent and nowShowing.
-      // TODO: [EG] Add the call to the 
-      var indexes = sel.indexSetForSource(view.get('content'));
-      var first = indexes.firstObject();
-      var widgetView = view.itemViewForContentIndex(first);
-      isLocked = widgetView.get('isLocked');
-    }
-    return !isLocked;
-  },
-  
-  /**
-    Returns a the SCUI.WIDGET_TYPE for dragging between Dashboard Columns
-    
-    @param view {SC.CollectionView} the collection view to begin dragging.
-    @returns {Array} array of supported data types.
-  */
-  collectionViewDragDataTypes: function(view) { 
-    return [SCUI.WIDGET_TYPE]; 
-  },
-  
-  /**
-    Called by a collection view when a drag concludes to give you the option
-    to provide the drag data for the drop.
-    
-    This method should be implemented essentially as you would implement the
-    dragDataForType() if you were a drag data source.  You will never be asked
-    to provide drag data for a reorder event, only for other types of data.
-    
-    The default implementation returns null.
-    
-    @param view {SC.CollectionView} 
-      the collection view that initiated the drag
-
-    @param dataType {String} the data type to provide
-    @param drag {SC.Drag} the drag object
-    @returns {Object} the data object or null if the data could not be provided.
-  */
-  collectionViewDragDataForType: function(view, drag, dataType) {  
-    var ret=null, sel;
-    
-    if (dataType === SCUI.WIDGET_TYPE) {
-      sel = view.get('selection');
-      ret = [];
-      if (sel) sel.forEach(function(x) { ret.push(x); }, this);
-    }
-    
-    return ret ;
-  },
-  
-  // ..........................................................
-  // DROP TARGET SUPPORT
-  //
-  
-  /**
-    Called once during a drag the first time view is entered. Return all 
-    possible drag operations OR'd together.
-    
-    @param {SC.CollectionView} view
-      the collection view that initiated the drag
-
-    @param {SC.Drag} drag
-      the drag object
-    
-    @param {Number} proposedDragOperations
-      proposed logical OR of allowed drag operations.
-
-    @returns {Number} the allowed drag operations. Defaults to op
-  */
-  collectionViewComputeDragOperations: function(view, drag, proposedDragOperations) {
-    if (drag.hasDataType(SCUI.WIDGET_TYPE)) {
-      var source = drag.get('source');
-      if (source && source.delegate === this) {
-        //console.log('SCUI.DashboardDelegate: collectionViewComputeDragOperations return DRAG_MOVE');
-        return SC.DRAG_MOVE;
-      }
-    }
-    //console.log('SCUI.DashboardDelegate: collectionViewComputeDragOperations return DRAG_NONE');
-    return SC.DRAG_NONE;
-  },
-  
-  /**
-    Called by the collection view to actually accept a drop.  This method will
-    only be invoked AFTER your validateDrop method has been called to
-    determine if you want to even allow the drag operation to go through.
-    
-    You should actually make changes to the data model if needed here and
-    then return the actual drag operation that was performed.  If you return
-    SC.DRAG_NONE and the dragOperation was SC.DRAG_REORDER, then the default
-    reorder behavior will be provided by the collection view.
-    
-    @param view {SC.CollectionView}
-    @param drag {SC.Drag} the current drag object
-    @param op {Number} proposed logical OR of allowed drag operations.
-    @param proposedInsertionIndex {Number} an index into the content array representing the proposed insertion point.
-    @param proposedDropOperation {String} the proposed drop operation.  Will be one of SC.DROP_ON, SC.DROP_BEFORE, or SC.DROP_ANY.
-    @returns the allowed drag operation.  Defaults to proposedDragOperation
-  */
-  collectionViewPerformDragOperation: function(view, drag, dragOp, proposedInsertionIndex, proposedDropOperaration) {
-    if (dragOp & SC.DRAG_REORDER){
-      this.collectionViewSetMetadataForContentIndex(view, null, proposedInsertionIndex);
-      // TODO: [EG] Add the callback to the model to update the position
-      return SC.DRAG_NONE; // allow reorder
-    }
-    
-    var widgets = drag.dataForType(SCUI.WIDGET_TYPE),
-        content   = view.get('content'),
-        len       = view.get('length'),
-        source    = drag.get('source'),
-        ret       = SC.DRAG_NONE;
-    
-    // only if data is available from drag
-    if (!widgets) return ret;
-    
-    // adjust the index to the location to insert and then add it
-    if (proposedDropOperaration & SC.DROP_AFTER) proposedInsertionIndex--;
-    if (proposedInsertionIndex>len) proposedInsertionIndex = len;
-    content.replace(proposedInsertionIndex, 0, widgets);
-    for (var i = 0, wLen = widgets.get('length'); i < wLen; i++ ){
-      //this.collectionViewInsertMetadataForContentIndex(view, null, proposedInsertionIndex+i);
-    }
-    
-    // if we can move, then remove widgets from the old one
-    if ( (dragOp & SC.DRAG_MOVE) && (content = source.get('content')) ) {
-      content.removeObjects(widgets);
-      ret = SC.DRAG_MOVE;
-    }
-    
-    return ret;
-  },
-  
-  /**
-    Renders a drag view for the passed content indexes. If you return null
-    from this, then a default drag view will be generated for you.
-    
-    @param {SC.CollectionView} view
-    @param {SC.IndexSet} dragContent
-    @returns {SC.View} view or null
-  */
-  collectionViewDragViewFor: function(view, dragContent) {
-     // find only the indexes that are in both dragContent and nowShowing.
-     var indexes = view.get('nowShowing').without(dragContent);
-     indexes = view.get('nowShowing').without(indexes);
-     
-     var first = indexes.firstObject();
-     var itemGhostView = view.itemViewForContentIndex(first);
-     
-     return itemGhostView;
   }
-});
+
+};
--- frameworks/dashboard/views/dashboard.js
+++ frameworks/dashboard/views/dashboard.js
@@ -1,171 +1,381 @@
 // ==========================================================================
 // SCUI.DashboardView
 // ==========================================================================
-sc_require('core');
-sc_require('views/dashboard_column');
+
+sc_require('views/widget_missing');
+sc_require('mixins/widget_overlay');
+sc_require('mixins/dashboard_delegate');
 
 /** @class
 
-  This is a management view that handles multiple StackViews for dashboard like architecture
+  This is an overridden SC.CollectionView as a container for dashboard widgets.
 
-  @extends SC.View
-  @author Evin Grano
-  @version 0.1
-  @since 0.1
+  @extends SC.CollectionView
+  @author Jonathan Lewis
 */
 
-SCUI.DashboardView = SC.View.extend(
-/** @scope SCUI.DashboardView.prototype */ {
+SCUI.DashboardView = SC.CollectionView.extend( SCUI.DashboardDelegate, {
+
+  // PUBLIC PROPERTIES
   
   classNames: ['scui-dashboard-view'],
   
-  /* @public: general params */
-  /* @array: 
-    {
-      exampleView: SC.View,
-      content: {
-        col: 0,
-        colIndex: 1,
-        name: 'name',
-        *size: {width: 100, height: 100} <= Optional, if your model controls the size of the widget
-      }
-    }
-  */
-  content: null,
-  columnWidth: 400,
-  exampleWidgetView: SCUI.WidgetView,
-  delegate: null,
+  acceptsFirstResponder: YES,
+  
+  allowsEmptySelection: YES,
+  allowsMultipleSelection: NO,
   
   /**
-    Accept drops for data other than reordering.
-    Set to true to allow of cross dashboard column inserting
-    
-    @type {Boolean}
+    Deletion of content via delete keys is not allowed as we have a special state to handle that.
   */
-  isDropTarget: YES,
+  canDeleteContent: NO,
   
-  /* @public: content param keys */
-  columnKey: 'col',
-  columnIndexKey: 'colIndex',
-  
-  /* @private */
-  _leftColumn: null,
-  _rightColumn: null,
-  _columnHeights: null,
-  
-  createChildViews: function(){
-    var view, params, childViews = [];
-    // Get the needed params
-    var cWidth = this.get('columnWidth') || 400;
-    var exampleWidgetView = this.get('exampleWidgetView') || SCUI.WidgetView ;
-    var delegate = this.get('delegate');
+  /**
+    A fallback item view that will only be used if the dashboard cannot
+    fetch a custom widget view for a dashboard item.  It is just a basic
+    view with an error message saying it can't find the correct widget view.
+  */
+  exampleView: SCUI.WidgetMissingView,
+
+  /**
+    The delegate responsible for handing out appropriate widget view classes for
+    widgets in this dashboard.  Checks for delegates in this order:
+      1. the 'delegate' property
+      2. the 'content' property, in case the content is also a SCUI.DashboardDelegate
+      3. this view itself
+  */
+  dashboardDelegate: function() {
+    var del = this.get('delegate'), content = this.get('content');
+    return this.delegateFor('isDashboardDelegate', del, content, this);
+  }.property('delegate', 'content').cacheable(),
+
+  // PUBLIC METHODS
+
+  init: function() {
+    var del;
+
+    sc_super();
     
-    // First, create the Left Column
-    this._leftColumn = this.createChildView( SCUI.DashboardColumnView, {
-      layout: {left: 0, top: 0, bottom: 0, width: cWidth},
-      exampleView: exampleWidgetView,
-      widgetMaxWidth: cWidth,
-      rowSpacing: 10,
-      rowMargin: 5,
-      delegate: delegate,
-      columnIndex: 0
-    });
-    childViews.push(this._leftColumn);
-    
-    // First, create the Right Column
-    this._rightColumn = this.createChildView( SCUI.DashboardColumnView, {
-      layout: {right: 0, top: 0, bottom: 0, width: cWidth},
-      exampleView: exampleWidgetView,
-      rowSpacing: 10,
-      rowMargin: 5,
-      widgetMaxWidth: cWidth,
-      delegate: delegate,
-      columnIndex: 1
-    });
-    childViews.push(this._rightColumn);
-    
-    this.set('childViews', childViews);
+    del = this.get('dashboardDelegate');
+    if (!del) {
+      this.set('dashboardDelegate', this);
+    }
+  },
+
+  /**
+    Just API for now...
+  */
+  beginManaging: function() {
+    // var childViews, len, view, i;
+    // 
+    // console.log('%@.beginManaging()'.fmt(this));
+    // 
+    // if (!this._isManaging) {
+    //   this._isManaging = YES;
+    // 
+    //   childViews = this.get('childViews') || [];
+    //   len = childViews.get('length');
+    //   for (i = 0; i < len; i++) {
+    //     view = childViews.objectAt(i);
+    //     view.set('deleteHandleIsVisible', YES);
+    //   }
+    // }
   },
   
-  didCreateLayer: function(){
-    this._updateWidgets();
+  /**
+    Just API for now...
+  */
+  endManaging: function() {
+    // var childViews, len, view, i;
+    // 
+    // console.log('%@.endManaging()'.fmt(this));
+    // if (this._isManaging) {
+    //   childViews = this.get('childViews') || [];
+    //   len = childViews.get('length');
+    //   for (i = 0; i < len; i++) {
+    //     view = childViews.objectAt(i);
+    //     view.set('deleteHandleIsVisible', NO);
+    //   }
+    //   
+    //   this._isManaging = NO;
+    // }
   },
+
+  /**
+    Returns the item view for the content object at the specified index. Call
+    this method instead of accessing child views directly whenever you need 
+    to get the view associated with a content index.
+
+    Although this method take two parameters, you should almost always call
+    it with just the content index.  The other two parameters are used 
+    internally by the CollectionView.
+    
+    If you need to change the way the collection view manages item views
+    you can override this method as well.  If you just want to change the
+    default options used when creating item views, override createItemView()
+    instead.
   
-  contentDidChange: function(){
-    this._updateWidgets();
-  }.observes('content'),
-  
-  _updateWidgets: function(){
-    console.log('Dashboard: Updating Widgets...');
-    var c = this.get('content');
-    if (c && c.length > 0) {
-      
-      var leftColWidgets = [];
-      var rightColWidgets = [];
-      
-      // Widget Keys
-      var colKey = this.get('columnKey');
-      var colIdxKey = this.get('columnIndexKey');
-            
-      var currWidget, col, idx;
-      var currentIndex = 0;
-      for(var i = 0, len = c.length; i < len; i++){
-        currWidget = c[i];
-        // Now grab the column and Column index
-        col = currWidget.get(colKey);
-        idx = currWidget.get(colIdxKey);
-        
-        // Do some default clean up in case we come across a widget that doesn't have a position
-        if (idx === undefined) idx = currentIndex+1;
-        if (!col && (idx > currentIndex)) {
-          col = 0;
-          currentIndex = idx;
-        }
-        
-        if (col < 1){
-          leftColWidgets[idx] = currWidget;
-        }
-        else {
-          rightColWidgets[idx] = currWidget;
+    Note that if you override this method, then be sure to implement this 
+    method so that it uses a cache to return the same item view for a given
+    index unless "force" is YES.  In that case, generate a new item view and
+    replace the old item view in your cache with the new item view.
+    
+    Also, although "internal only," if called with "rebuild," the <em>caller</em> 
+    is the one responsible for removing any old node from the parent node.
+
+    @param {Number} idx the content index
+    @param {Boolean} rebuild internal use only
+    @returns {SC.View} instantiated view
+  */
+  itemViewForContentIndex: function(idx, rebuild) {
+    // return from cache if possible
+    var content   = this.get('content'),
+        itemViews = this._sc_itemViews,
+        item = content.objectAt(idx),
+        del  = this.get('contentDelegate'),
+        dashboardDelegate = this.get('dashboardDelegate'),
+        groupIndexes = del.contentGroupIndexes(this, content),
+        isGroupView = NO,
+        key, ret, E, layout, layerId;
+
+    // use cache if available
+    if (!itemViews) itemViews = this._sc_itemViews = [] ;
+    if (!rebuild && (ret = itemViews[idx])) return ret ; 
+    
+    // make sure to get rid of the cached one if we aren't using it
+    if (itemViews[idx]) {
+      itemViews[idx].destroy();
+      delete itemViews[idx];
+    }
+    
+    // otherwise generate...
+    
+    // first, determine the class to use
+    isGroupView = groupIndexes && groupIndexes.contains(idx);
+
+    // ask the delegate for the appropriate widget view
+    if (dashboardDelegate) {
+      E = dashboardDelegate.dashboardWidgetViewFor(this, content, idx);
+    }
+
+    // if it wasn't found, fall back on the standard example view
+    if (!E) {
+      if (isGroupView) isGroupView = del.contentIndexIsGroup(this, content,idx);
+      if (isGroupView) {
+        key  = this.get('contentGroupExampleViewKey');
+        if (key && item) E = item.get(key);
+        if (!E) E = this.get('groupExampleView') || this.get('exampleView');
+
+      } else {
+        key  = this.get('contentExampleViewKey');
+        if (key && item) E = item.get(key);
+        if (!E) E = this.get('exampleView');
+      }
+    }
+
+    // collect some other state
+    var attrs = this._TMP_ATTRS;
+    attrs.contentIndex = idx;
+    attrs.content      = item ;
+    attrs.owner        = attrs.displayDelegate = this;
+    attrs.parentView   = this.get('containerView') || this ;
+    attrs.page         = this.page ;
+    attrs.layerId      = this.layerIdFor(idx, item);
+    attrs.isEnabled    = del.contentIndexIsEnabled(this, content, idx);
+    attrs.isSelected   = del.contentIndexIsSelected(this, content, idx);
+    attrs.outlineLevel = del.contentIndexOutlineLevel(this, content, idx);
+    attrs.disclosureState = del.contentIndexDisclosureState(this, content, idx);
+    attrs.isGroupView  = isGroupView;
+    attrs.isVisibleInWindow = this.isVisibleInWindow;
+    if (isGroupView) attrs.classNames = this._GROUP_COLLECTION_CLASS_NAMES;
+    else attrs.classNames = this._COLLECTION_CLASS_NAMES;
+    
+    layout = this.layoutForContentIndex(idx);
+    if (layout) {
+      attrs.layout = layout;
+    } else {
+      delete attrs.layout ;
+    }
+    
+    ret = this.createItemView(E, idx, attrs);
+    itemViews[idx] = ret ;
+    return ret ;
+  },
+
+  /**
+    Calls the base CollectionView.createItemView() method first, then
+    overrides the newly created itemView's layout.
+  */
+  createItemView: function(exampleClass, idx, attrs) {
+    var itemView, content, frame, position, size;
+    var finalLayout = { left: 20, top: 20, width: 400, height: 200 }; // default, will overwrite if possible
+
+    console.log('%@.createItemView(%@)'.fmt(this, idx));
+
+    attrs = SC.merge(attrs, SCUI.WidgetOverlay); // Mix in the widget overlay view additions    
+    itemView = sc_super(); // call base method to create the view so we have valid frame info; we'll then override the layout
+
+    if (itemView) {
+      content = itemView.get('content');
+
+      // width & height: first fetch from item if possible
+      if (content) {
+        size = content.get('size');
+        if (size) {
+          finalLayout.width = parseFloat(size.width) || 400;
+          finalLayout.height = parseFloat(size.height) || 200;
         }
       }
+
+      // width & height: if that didn't work, try using the itemView's frame size
+      if (!size) {
+        frame = itemView.get('frame');
+        if (frame) {
+          finalLayout.width = frame.width || 200;
+          finalLayout.height = frame.height || 100;
+        }
+        else {
+          console.warn('%@. ItemView %@ does not have a valid layout.'.fmt(this, idx));
+        }
+      }
+
+      // position
+      position = this._getItemPosition(content); // null if not found
+      if (position) { // if we found a stored position, use it
+        finalLayout.left = position.x;
+        finalLayout.top = position.y;
+      }
+      else if (content) { // if it had no position, use a default, and save it on the dashboard element
+        this._setItemPosition(content, { x: finalLayout.left, y: finalLayout.top });
+      }
+
+      // finally, override the layout with the standard style
+      itemView.set('layout', finalLayout);
+    }
+
+    return itemView;
+  },
+  
+  mouseDown: function(evt) {
+    var itemView;
+
+    sc_super();
+
+    // Since a mouse down could be the start of a drag, save all
+    // the data we'll need for it
+    this._dragData = null;
+    if (evt && evt.which === 1) { // left mouse button
+      itemView = this.itemViewForEvent(evt);
+      if (itemView && !itemView.getPath('content.isLocked')) { // only start dragging if widget isn't locked
+        this._dragData = SC.clone(itemView.get('layout'));
+        this._dragData.pageX = evt.pageX;
+        this._dragData.pageY = evt.pageY;
+        this._dragData.view = itemView;
+        this._dragData.didMove = NO; // haven't moved yet
+      }
+    }
+    
+    return YES;
+  },
+  
+  mouseUp: function(evt) {
+    var content, frame;
+
+    sc_super();
+
+    // If this mouse up comes at the end of a drag of a widget
+    // view, try to update the widget's model with new position
+    if (this._dragData && this._dragData.didMove) {
+      content = this._dragData.view.get('content');
+      frame = this._dragData.view.get('frame');
+
+      // try to update the widget data model with the new position
+      if (content && frame) {
+        this._setItemPosition(content, { x: frame.x, y: frame.y });
+      }
+    }
+
+    this._dragData = null; // clean up
+    return YES;
+  },
+  
+  mouseDragged: function(evt) {
+    var d, dX, dY;
+
+    sc_super();
+
+    // We're in the middle of a drag, so adjust the view using the current drag delta
+    if (this._dragData) {
+      this._dragData.didMove = YES; // so that mouseUp knows whether to report the new position.
       
-      // Compact the arrays
-      leftColWidgets = this._compact(leftColWidgets);
-      rightColWidgets = this._compact(rightColWidgets);
-      
-      this._leftColumn.set('content', leftColWidgets);
-      this._rightColumn.set('content', rightColWidgets);
+      d = this._dragData;
+      dX = evt.pageX - d.pageX;
+      dY = evt.pageY - d.pageY;
+
+      this._adjustViewLayoutOnDrag(d.view, d.zoneX, d.zoneY, dX, d, 'left', 'right', 'centerX', 'width');
+      this._adjustViewLayoutOnDrag(d.view, d.zoneY, d.zoneX, dY, d, 'top', 'bottom', 'centerY', 'height');
+    }
+    return YES;
+  },
+  
+  // PRIVATE METHODS
+  
+  /**
+    A utility method that adjust's the view's layout given a movement delta.
+    It is smart about finding the view's own layout style and adjusting that.
+  */
+  _adjustViewLayoutOnDrag: function(view, curZone, altZone, delta, i, headKey, tailKey, centerKey, sizeKey) {
+    // collect some useful values...
+    var inAltZone = false; //(altZone === HEAD_ZONE) || (altZone === TAIL_ZONE);
+    var head = i[headKey], tail = i[tailKey], center = i[centerKey], size = i[sizeKey];
+    //this block determines what layout coordinates you have (top, left, centerX,centerY, right, bottom)
+    //and adjust the view depented on the delta
+    if (!inAltZone && !SC.none(size)) {
+      if (!SC.none(head)) {
+        view.adjust(headKey, head + delta);
+      } else if (!SC.none(tail)) {
+        view.adjust(tailKey, tail - delta) ;
+      } else if (!SC.none(center)) {
+        view.adjust(centerKey, center + delta);
+      }
     }
   },
   
-  adjustColumnHeight: function(columnIndex, newHeight){
-    //console.log('SCUI.DashboardView: adjustColumnHeight with %@'.fmt(newHeight));
-    if (!this._columnHeights) this._columnHeights = [];
-    if (columnIndex >= 0) this._columnHeights[columnIndex] = newHeight;
-    
-    var max = newHeight, curr;
-    for (var i = 0, len = this._columnHeights.length; i < len; i++){
-      curr = this._columnHeights[i];
-      max = max < curr ? curr : max;
+  /**
+    Encapsulates the standard way the dashboard attempts to extract the last
+    position from the dashboard element.
+    Returns null if unsuccessful.
+  */
+  _getItemPosition: function(item) {
+    var posKey, pos;
+
+    if (item) {
+      posKey = item.get('positionKey') || 'position';
+      pos = item.get(posKey);
+      if (pos) {
+        return { x: (parseFloat(pos.x) || 0), y: (parseFloat(pos.y) || 0) };
+      }
     }
-    this.adjust('height', max);
+
+    return null;
   },
   
-  /****************************************************
-  * @private
-  * Function for getting rid of all non-valid objects
-  ****************************************************/
-  _compact: function(arrayToCompact){
-    var compacted = [];
-    if(!arrayToCompact) return arrayToCompact;
-    var curr;
-    for( var i = 0, len = arrayToCompact.length; i < len; i++){
-      curr = arrayToCompact[i];
-      if(curr) compacted.push(curr);
+  /**
+    Encapsulates the standard way the dashboard attempts to store the last
+    position on a dashboard element.
+  */
+  _setItemPosition: function(item, pos) {
+    var posKey;
+
+    if (item) {
+      posKey = item.get('positionKey') || 'position';
+      item.set(posKey, pos);
     }
-    
-    return compacted;
-  }
-    
+  },
+
+  // PRIVATE PROPERTIES
+  
+  _dragData: null, // data about the itemView currently being dragged; null when not dragging
+  _isManaging: NO
+
 });

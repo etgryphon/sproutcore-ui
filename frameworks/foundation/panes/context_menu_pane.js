d10de795b72a8a920c7360dffbc985a43b5358c7
Joshua Holt <holt.josh@gmail.com>
2010-01-20 15:32:14 -0500

    [In progress] enabling context menu for actual mouse position not relative to an anchor.

--- frameworks/foundation/panes/context_menu_pane.js
+++ frameworks/foundation/panes/context_menu_pane.js
@@ -45,7 +45,13 @@
 */
 
 SCUI.ContextMenuPane = SC.MenuPane.extend({
-
+  
+  /**
+    This flag is for the special case when the anchor view is using static
+    layout, i.e ( SC.StackedView, or mixn SC.StaticLayout).
+  */
+  usingStaticLayout: NO,
+  
   /**
     If evt is a right-click, this method pops up a context menu next to the mouse click.
     Returns YES if we popped up a context menu, otherwise NO.
@@ -53,7 +59,7 @@ SCUI.ContextMenuPane = SC.MenuPane.extend({
     AnchorView must be a valid SC.View object.
   */
   popup: function(anchorView, evt) {
-    if (!anchorView || !anchorView.isView) return NO;
+    if ((!anchorView || !anchorView.isView) && !this.get('usingStaticLayout')) return NO;
   
     if (evt && evt.button && (evt.which === 3 || (evt.ctrlKey && evt.which === 1))) {
   
@@ -64,13 +70,22 @@ SCUI.ContextMenuPane = SC.MenuPane.extend({
   
       // calculate offset needed from top-left of anchorViewOrElement to position the menu
       // pane next to the mouse click location
-      var offsetX = 0, offsetY = 0;
-      var pv = anchorView.get('parentView');
-      var frame = anchorView.get('frame');
-      var globalFrame = pv ? pv.convertFrameToView(frame, null) : frame;
-      if(globalFrame){
-        offsetX = evt.pageX - globalFrame.x;
-        offsetY = evt.pageY - globalFrame.y;
+      if (!this.get('usingStaticLayout')) {
+        var offsetX = 0, offsetY = 0;
+        var pv = anchorView.get('parentView');
+        var frame = anchorView.get('frame');
+        var globalFrame = pv ? pv.convertFrameToView(frame, null) : frame;
+        if(globalFrame){
+          offsetX = evt.pageX - globalFrame.x;
+          offsetY = evt.pageY - globalFrame.y;
+        }
+      }else{
+        var parentView = anchorView.get('parentView');
+        var pFrame = parentView.get('frame');
+        // NOTE [jh2] this is wrong b/c I have to use the frame of the parent view...
+        var gFrame = pv ? parentView.convertFrameToView(pFrame, null) : pFrame;
+        offsetX = evt.pageX - gFrame.x;
+        offsetY = evt.pageY - gFrame.y;
       }
   
       var anchor = anchorView.isView ? anchorView.get('layer') : anchorView;

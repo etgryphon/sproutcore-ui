6551411c51bd46e4c76d2f9da3cb2dfcf7f562bd
Matthew Grantham <matthew.grantham@eloqua.com>
2010-02-24 15:24:29 -0500

    fixes for missing widget view

--- frameworks/dashboard/views/missing_widget.js
+++ frameworks/dashboard/views/missing_widget.js
@@ -12,19 +12,17 @@ SCUI.MissingWidgetView = SC.View.extend( SC.Border, {
   // PUBLIC PROPERTIES
 
   layout: { left: 0, right: 0, top: 0, bottom: 0 },
-
-  backgroundColor: '#769dc1',
-
-  borderStyle: SC.BORDER_BLACK,
-
-  message: "Widget view is missing".loc(),
+  
+  message: "Widget is missing or broken. Please remove and replace this widget using the plus button in the bottom left.".loc(),
+  
+  classNames: 'missing-widget'.w(),
 
   createChildViews: function() {
     var childViews = [];
 
     childViews.push( this.createChildView(
       SC.LabelView.design({
-        layout: { left: 0, right: 0, centerY: 0, height: 30 },
+        layout: { left: 10, right: 10, centerY: 0, height: 40 },
         textAlign: SC.ALIGN_CENTER,
         value: this.get('message')
       })

e054cd0f89501319087022391c66642bf5da112d
Mike Ball <mike.ball3@gmail.com>
2010-05-05 17:20:25 -0400

    updates to bar_graph

--- frameworks/sai/views/bar_graph.js
+++ frameworks/sai/views/bar_graph.js
@@ -20,38 +20,50 @@ SCUI.BarGraph = SC.View.extend(
   */
   content: null,
   
+  color: 'red',
   
-  render: function(context, firstTime){
+  _content_changed: function(){
+    if(this.get('content') && this.get('isVisible')){
+      console.log(this.get('content'));
+      if(this._raphael) this._raphael.remove();
+      this._render_view();
+    }
   },
   
   didAppendToDocument: function(){    
     // this is where colorpickers created
-    var layer = this.$().get(0);
-    var r = Raphael(layer),
-    fin = function () {
-      this.flag = r.g.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
-    },
-    fout = function () {
-      this.flag.animate({opacity: 0}, 300, function () {this.remove();});
-    },
-    fin2 = function () {
-      var y = [], res = [];
-      for (var i = this.bars.length; i--;) {
-        y.push(this.bars[i].y);
-        res.push(this.bars[i].value || "0");
-      }
-      this.flag = r.g.popup(this.bars[0].x, Math.min.apply(Math, y), res.join(", ")).insertBefore(this);
-    },
-    fout2 = function () {
-      this.flag.animate({opacity: 0}, 300, function () {this.remove();});
-    };
-    
-    r.g.hbarchart(0, 0, 300, 220, [this.get('content')]).hover(fin, fout);
-    this._raphael = r;
+    var that = this;
+    this.invokeLater(function(){that._render_view();});
   },
   
   willDestroyLayer: function(){
     if(this._raphael) this._raphael.remove();
+  },
+  
+  _render_view: function(){
+    if(!this._raphael && this.$().get(0)){    
+      var layer = this.$().get(0);
+      var r = Raphael(layer),
+      fin = function () {
+        this.flag = r.g.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
+      },
+      fout = function () {
+        this.flag.animate({opacity: 0}, 200, function () {this.remove();});
+      },
+      fin2 = function () {
+        var y = [], res = [];
+        for (var i = this.bars.length; i--;) {
+          y.push(this.bars[i].y);
+          res.push(this.bars[i].value || "0");
+        }
+        this.flag = r.g.popup(this.bars[0].x, Math.min.apply(Math, y), res.join(", ")).insertBefore(this);
+      },
+      fout2 = function () {
+        this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+      };
+      r.g.hbarchart(0, 0, 300, 220, this.get('content'), {stacked: true}).hover(fin, fout);
+      this._raphael = r;
+    }
   }
   
 });

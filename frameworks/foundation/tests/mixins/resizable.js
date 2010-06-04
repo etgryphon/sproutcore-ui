fd0a3fe0cb0ea28b0bd6bbae7804eaa4621c8f9d
Brandon Blatnick <brandon.blatnick@eloqua.com>
2010-06-04 14:21:29 -0400

    Made a resizable palette pane

--- frameworks/foundation/tests/mixins/resizable.js
+++ frameworks/foundation/tests/mixins/resizable.js
@@ -17,11 +17,26 @@ SC.View.design({
   })
 });
 
+var palette = 
+SC.PalettePane.create({
+  layout: { left: 0, height: 100, width: 100 },
+  contentView: SC.View.extend({
+    backgroundColor: 'blue',
+    childViews: 'thumb'.w(),
+    thumb: SC.View.design(SCUI.Resizable, {
+      layout: {bottom: 0, right: 0, height: 25, width: 25},
+      backgroundColor: 'red',
+      viewToResizeBinding: SC.Binding.oneWay('.parentView.parentView') 
+    })
+  })
+});
+
 var pane = SC.ControlTestPane.design()
 .add("basic", basic);
       
 pane.show(); // add a test to show the test pane
 window.pane = pane ;
+palette.append();
 
 // ..........................................................
 // TEST CASES

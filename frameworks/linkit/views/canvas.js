4c808d23cd2e14454530bd828ac4e84c47ad55ff
Jonathan Lewis <jslewis1@gmail.com>
2010-07-27 15:04:56 -0400

    Removing overridden version of itemViewForContentIndex in LinkIt.CanvasView because it had diverged too far from SC.CollectionView's version to work, and it is no longer necessary.  And it's not a good idea to override such a big, central method anyway, duplicating all that code.

--- frameworks/linkit/views/canvas.js
+++ frameworks/linkit/views/canvas.js
@@ -53,10 +53,6 @@ LinkIt.CanvasView = SC.CollectionView.extend({
   contextMenuAction: null,
 
   /**
-  */
-  nodeViewDelegate: null,
-  
-  /**
     How close you have to click to a line before it is considered a hit
   */
   LINK_SELECTION_FREEDOM: 6,
@@ -142,96 +138,6 @@ LinkIt.CanvasView = SC.CollectionView.extend({
       viewIndex[nodeID] = view;
     }
     this._nodeViewIndex = viewIndex;
-  },
-
-  /**
-    Returns the item view for the content object at the specified index. Call
-    this method instead of accessing child views directly whenever you need 
-    to get the view associated with a content index.
-
-    Although this method take two parameters, you should almost always call
-    it with just the content index.  The other two parameters are used 
-    internally by the CollectionView.
-    
-    If you need to change the way the collection view manages item views
-    you can override this method as well.  If you just want to change the
-    default options used when creating item views, override createItemView()
-    instead.
-  
-    Note that if you override this method, then be sure to implement this 
-    method so that it uses a cache to return the same item view for a given
-    index unless "force" is YES.  In that case, generate a new item view and
-    replace the old item view in your cache with the new item view.
-
-    @param {Number} idx the content index
-    @param {Boolean} rebuild internal use only
-    @returns {SC.View} instantiated view
-  */
-  itemViewForContentIndex: function(idx, rebuild) {
-    //console.log('%@.itemViewForContentIndex(%@, %@)'.fmt(this, idx, rebuild));
-    // return from cache if possible
-    var content   = this.get('content'),
-        itemViews = this._sc_itemViews,
-        item = content.objectAt(idx),
-        del  = this.get('contentDelegate'),
-        groupIndexes = del.contentGroupIndexes(this, content),
-        isGroupView = NO,
-        key, ret, E, layout, layerId,
-        nodeViewDelegate;
-
-    // use cache if available
-    if (!itemViews) itemViews = this._sc_itemViews = [] ;
-    if (!rebuild && (ret = itemViews[idx])) return ret ; 
-
-    // otherwise generate...
-    
-    // first, determine the class to use
-    isGroupView = groupIndexes && groupIndexes.contains(idx);
-    if (isGroupView) isGroupView = del.contentIndexIsGroup(this, content,idx);
-    if (isGroupView) {
-      key  = this.get('contentGroupExampleViewKey');
-      if (key && item) E = item.get(key);
-      if (!E) E = this.get('groupExampleView') || this.get('exampleView');
-
-    } else {
-      key  = this.get('contentExampleViewKey');
-      if (key && item) E = item.get(key);
-
-      // Ask the example view delegate if there is one
-      if (!E && (nodeViewDelegate = this.get('nodeViewDelegate'))) {
-        E = nodeViewDelegate.exampleViewForNode(item);
-      }
-  
-      if (!E) E = this.get('exampleView');
-    }
-
-    // collect some other state
-    var attrs = this._TMP_ATTRS;
-    attrs.contentIndex = idx;
-    attrs.content      = item ;
-    attrs.owner        = attrs.displayDelegate = this;
-    attrs.parentView   = this.get('containerView') || this ;
-    attrs.page         = this.page ;
-    attrs.layerId      = this.layerIdFor(idx, item);
-    attrs.isEnabled    = del.contentIndexIsEnabled(this, content, idx);
-    attrs.isSelected   = del.contentIndexIsSelected(this, content, idx);
-    attrs.outlineLevel = del.contentIndexOutlineLevel(this, content, idx);
-    attrs.disclosureState = del.contentIndexDisclosureState(this, content, idx);
-    attrs.isGroupView  = isGroupView;
-    attrs.isVisibleInWindow = this.isVisibleInWindow;
-    if (isGroupView) attrs.classNames = this._GROUP_COLLECTION_CLASS_NAMES;
-    else attrs.classNames = this._COLLECTION_CLASS_NAMES;
-    
-    layout = this.layoutForContentIndex(idx);
-    if (layout) {
-      attrs.layout = layout;
-    } else {
-      delete attrs.layout ;
-    }
-    
-    ret = this.createItemView(E, idx, attrs);
-    itemViews[idx] = ret ;
-    return ret ;
   },
 
   /**

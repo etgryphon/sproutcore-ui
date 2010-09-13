a4763ad31292ed77ffd44170a5308d14a114d3b0
Mohammed Taher <mohammed.taher@eloqua.com>
2010-09-13 12:45:39 -0400

    moving files around

--- frameworks/calendar/views/calendar.js
+++ frameworks/calendar/views/calendar.js
@@ -2,224 +2,130 @@
 // SCUI.CalendarView
 // ==========================================================================
 
-sc_require('core');
-sc_require('views/date');
-
-/** @class
-
-  This is the calendar view that creates the calendar block with the dates
-
-  @extends SC.View
-  @author Evin Grano
-  @version 0.1
-  @since 0.1
-*/
-
-SCUI.CalendarView1 = SC.View.extend(
-/** @scope SCUI.CalendarView.prototype */ {
-  classNames: ['scui-calendar'],
+SCUI.CalendarView = SC.View.extend({
   
-  // Necessary Elements
-  monthStartOn: null,
-  content: null,
-  titleKey: 'title',
-  dateKey: 'date',
-  dateSize: {width: 100, height: 100},
-  dateBorderWidth: 0,
-  headerHeight: null,
-  weekdayHeight: 20,
-  exampleDateView: SCUI.DateView,
+  weekdayStrings: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  
+  monthStartOn: SC.DateTime.create({day: 1}),
   selectedDate: null,
   
-  // Optional Parameters
-  weekdayStrings: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-  monthStrings: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-  
-  _dateGrid: [],
-  
-  init: function(){
-    var monthStartOn = this.get('monthStartOn') || SC.DateTime.create({day: 1});
-    this.set('monthStartOn', monthStartOn);
-    // Size stuff
-    var dateSize = this.get('dateSize');
-    var dateWidth = dateSize.width || 100;
-    var dateHeight = dateSize.height || 100;
-    var headerHeight = this.get('headerHeight') || dateHeight/4;
-    if (headerHeight < 20) headerHeight = 20;
-    this.set('dateSize', {width: dateWidth, height: dateHeight});
-    this.set('headerHeight', headerHeight);
-    this._dateGrid = []; 
-    sc_super();
-  },
-  
-  awake: function(){
-    this.resetToSelectedDate();
-    sc_super();
-  },
+  displayProperties: ['monthStartOn'],
   
   resetToSelectedDate: function(){
     var selectedDate = this.get('selectedDate');
-    if (selectedDate) this.set('monthStartOn', selectedDate.adjust({day: 1}));
+    if (selectedDate) this.set('monthStartOn', selectedDate.adjust({ day: 1 }));
   },
   
-  // display properties that should automatically cause a refresh.
-  displayProperties: ['monthStartOn', 'selectedDate', 'titleKey', 'dateKey', 'content', 'content.[]'],
+  mouseDown: function(evt) {
+    var date = this._parseSelectedDate(evt.target.id);
+    if (date) this.set('selectedDate', date);
+    
+    if (evt.target.className === 'button previous') {
+      this.$('.button.previous').addClass('active');
+    } else if (evt.target.className === 'button next') {
+      this.$('.button.next').addClass('active');
+    }
+    return YES;
+  },
   
-  render: function(context, firstTime){
-    //console.log('Render called');
-    var borderWidth = this.get('dateBorderWidth');
-    var dateSize = this.get('dateSize');
-    var trueWidth = (2*borderWidth)+dateSize.width;
-    var trueHeight = (2*borderWidth)+dateSize.height;
-    var headerHeight = this.get('headerHeight');
-    var weekdayHeight = this.get('weekdayHeight');
-    var totalWidth = trueWidth*7;
-    var totalHeight = (trueHeight*6) + (headerHeight+weekdayHeight);
-    var layout = this.get('layout');
-    layout = SC.merge(layout, {width: totalWidth, height: totalHeight});
-    this.set('layout', layout);
-    // Date stuff
+  mouseUp: function(evt) {
     var monthStartOn = this.get('monthStartOn');
-    var weekdayStrings = this.get('weekdayStrings');
-    var monthStrings = this.get('monthStrings');
-    var currMonth = monthStartOn.get('month');
-    var currYear = monthStartOn.get('year');
-    if (firstTime){
-      var headerStr = '<div class="month_header" style="position: absolute; left: %@1px; right: %@1px; height: %@2px;">%@3 %@4</div>';
-      context.push( headerStr.fmt(trueWidth, headerHeight, monthStrings[currMonth-1].loc(), currYear) );
-      // Construct the Date Strings
-      var startLeft = 0;
-      for( var i = 0; i < 7; i++){
-        context.push(
-          '<div class="weekday" style="position: absolute; left: %@1px; top: %@2px; width: %@3px; height: 20px;">%@4</div>'.fmt( 
-            startLeft,
-            headerHeight,
-            dateSize.width,
-            weekdayStrings[i].loc()
-          )
-        );
-        startLeft += trueWidth;
-      }
-    }
-    else {
-      this.$('.month_header').text('%@ %@'.fmt( monthStrings[currMonth-1].loc(), currYear ));
-    }
-    this._updateDates();
-    sc_super();
-  },
-  
-  /**
-    Create the main childviews
-  */
-  createChildViews: function() {
-    var childViews = [], view=null;
     
-    var borderWidth = this.get('dateBorderWidth');
-    var dateSize = this.get('dateSize');
-    var trueWidth = (2*borderWidth)+dateSize.width;
-    var trueHeight = (2*borderWidth)+dateSize.height;
-    var headerHeight = this.get('headerHeight');
-    
-    // Create the Next And Previous Month Buttons
-    view = this.createChildView( 
-      SC.View.design( SCUI.SimpleButton, {
-        classNames: ['scui-cal-button', 'previous-month-icon'],
-        layout: {left: 5, top: 6, width: 8, height: 9},
-        target: this,
-        action: 'previousMonth'
-      }),
-      { rootElementPath: [0] }
-    );
-    childViews.push(view);
-    
-    // Next Month Button
-    view = this.createChildView( 
-      SC.View.design(SCUI.SimpleButton, {
-        classNames: ['scui-cal-button', 'next-month-icon'],
-        layout: {right: 5, top: 6, width: 8, height: 9},
-        target: this,
-        action: 'nextMonth'
-      }),
-      { rootElementPath: [1] }
-    );
-    childViews.push(view);
-    
-    // Now, loop and make the date grid
-    var startLeft = borderWidth;
-    var startTop = headerHeight + this.get('weekdayHeight');
-    var exampleView = this.get('exampleDateView');
-    for( var i = 0; i < 42; i++){
-      view = this.createChildView( 
-        exampleView.design({
-          layout: {left: startLeft, top: startTop, width: dateSize.width, height: dateSize.height},
-          timing: SCUI.PAST,
-          calendarView: this,
-          date: i
-        }),
-        { rootElementPath: [i+2] }
-      );
-      this._dateGrid.push(view);
-      childViews.push(view);
+    if (evt.target.className === 'button previous active') {
+      this.set('monthStartOn', monthStartOn.advance({month: -1}));
+      this.$('.button.previous').removeClass('active');
+    } else if (evt.target.className === 'button next active') {
+      this.set('monthStartOn', monthStartOn.advance({month: 1}));
+      this.$('.button.next').removeClass('active');
       
-      // Increment the position
-      if (((i+1) % 7) === 0) {
-        startTop += trueHeight;
-        startLeft = borderWidth;
-      }
-      else {
-        startLeft += trueWidth;
-      }
     }
-    this.set('childViews', childViews);
-    return this;
+    return YES;
   },
   
-  nextMonth: function() {
+  render: function(context, firstTime) {
     var monthStartOn = this.get('monthStartOn');
-    var nextMonth = monthStartOn.advance({month: 1});
-    this.set('monthStartOn', nextMonth);
-    this.displayDidChange();
-  },
-  
-  previousMonth: function() {
-    var monthStartOn = this.get('monthStartOn');
-    var prevMonth = monthStartOn.advance({month: -1});
-    this.set('monthStartOn', prevMonth);
-    this.displayDidChange();
-  },
-  
-  _updateDates: function(){
-    var monthStartOn = this.get('monthStartOn');
-    var month = monthStartOn.get('month');
     var startDay = monthStartOn.get('dayOfWeek');
     var currDate = monthStartOn.advance({day: -startDay});
-    var today = SC.DateTime.create();
-    var selectedDate = this.get('selectedDate');
-    var isSelected, timing;
-    for (var gIdx = 0; gIdx < 42; gIdx++)
-    {
-      if (gIdx < startDay){
-        this._dateGrid[gIdx].set('timing', SCUI.PAST);
-      }
-      else if(currDate.get('month') === month){
+    var selDate = this.get('selectedDate');
+    var todaysDate = SC.DateTime.create();
+    var weekdayStrings = this.get('weekdayStrings');
+    var classNames, uniqueDayIdentifier;
+    
+    context.push( '<div class="calendar-header">',
+                    '<div class="month">%@</div>'.fmt(SC.RenderContext.escapeHTML(monthStartOn.toFormattedString('%B %Y'))),
+                    '<div class="button previous"></div>',
+                    '<div class="button next"></div>',
+                  '</div>',
+                  '<div class="calendar-body">' );
+    
+    for (var i = 0; i < 7; i++) {
+      context .begin('div')
+              .addClass('day header')
+              .text(weekdayStrings[i])
+              .end();
+    }
+    
+    context.begin('div').addClass('grid');
+    
+    for (var gIdx = 0; gIdx < 42; gIdx++) {
+      uniqueDayIdentifier = this._createUniqueDayIdentifier(currDate);
+      
+      if (currDate.get('month') < monthStartOn.get('month') || currDate.get('month') > monthStartOn.get('month')) {
+        context .begin('div')
+                .attr('id', uniqueDayIdentifier)
+                .addClass('day')
+                .addClass('previous')
+                .text(currDate.get('day'))
+                .end();
         
-        // First Check to see if the Date is selected
-        if (selectedDate) {
-          isSelected = SC.DateTime.compareDate(currDate, selectedDate) === 0 ? YES : NO;
-          this._dateGrid[gIdx].set('isSelected', isSelected);
+      } else {
+        classNames = ['present'];
+        
+        if (currDate.get('day') === todaysDate.get('day') && 
+            currDate.get('month') === todaysDate.get('month') && 
+            currDate.get('year') === todaysDate.get('year')) {
+          classNames.push('today');
+        } 
+        
+        if (selDate && 
+            currDate.get('day') === selDate.get('day') && 
+            currDate.get('month') === selDate.get('month') && 
+            currDate.get('year') === selDate.get('year')) {
+          classNames.push('selected');
         }
         
-        // Check to see if the current date is to day
-        // or in the present month
-        timing = SC.DateTime.compareDate(currDate, today) === 0 ? SCUI.TODAY : SCUI.PRESENT;
-        this._dateGrid[gIdx].set('timing', timing);
+        context .begin('div')
+                .attr('id', uniqueDayIdentifier)
+                .addClass('day')
+                .addClass(classNames.join(' '))
+                .text(currDate.get('day'))
+                .end();
       }
-      else {
-        this._dateGrid[gIdx].set('timing', SCUI.FUTURE);
+      
+      currDate = currDate.advance({ day: 1 });
+    }
+    
+    context.push('</div>', '</div>');
+  },
+  
+  _createUniqueDayIdentifier: function(currDate) {
+    var day = currDate.get('day');
+    var month = currDate.get('month');
+    var year = currDate.get('year');
+    return 'scuidate-%@-%@-%@-%@'.fmt(this.get('layerId'), day, month, year);
+  },
+  
+  _parseSelectedDate: function(dateIdentifier) {
+    if (!SC.empty(dateIdentifier)) {
+      var dataArray = dateIdentifier.split('-');
+      if (dataArray.length === 5 && dataArray[0] === 'scuidate' && dataArray[1] === this.get('layerId')) {
+        var day = dataArray[2];
+        var month = dataArray[3];
+        var year = dataArray[4];
+        return SC.DateTime.create({ day: day, month: month, year: year });
       }
-      this._dateGrid[gIdx].set('date', currDate);
-      currDate = currDate.advance({day: 1});
-    } 
+    }
+    return null;
   }
+  
 });
-
